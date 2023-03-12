be0c319ff15a8da11df5a429e31647b68c5b403a
Abdullah-146 <abdullah.mumtaz65@gmail.com>
2023-03-13 01:05:41 +0500

    cc

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!ccv</h1>;
 }
